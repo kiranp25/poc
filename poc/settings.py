6fb526969d588a2a9474d3d2f5b890b4793a7e52
kiranp25 <161411771+kiranp25@users.noreply.github.com>
2024-05-10 18:04:30 +0530

    Update settings.py

--- poc/settings.py
+++ poc/settings.py
@@ -149,7 +149,7 @@ MEDIA_URL = '/media/'
 # EMAIL_USE_TLS = True
 # EMAIL_PORT = 587
 # EMAIL_HOST_USER = 'kiran.p@olatechs.com'#sender's email-id
-# EMAIL_HOST_PASSWORD = '3LuQmA8f8=' #password associated with above email-id
+# EMAIL_HOST_PASSWORD = '' #password associated with above email-id
 #
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
